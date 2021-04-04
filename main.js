eb1b00a0f2935251258367ce56bad5fd8d9c6b03
kctrnn <kctrnn@gmail.com>
2021-04-04 18:30:57 +0700

    custom output with pagination

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const queryString = require('query-string');
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
@@ -17,10 +18,39 @@ server.use(jsonServer.bodyParser);
 server.use((req, res, next) => {
   if (req.method === 'POST') {
     req.body.createdAt = Date.now();
+    req.body.updatedAt = Date.now();
+  } else if (req.method === 'PATCH') {
+    req.body.updatedAt = Date.now();
   }
   // Continue to JSON Server router
   next();
 });
+
+router.render = (req, res) => {
+  // check GET with pagination
+  // if yes, custom output
+  const headers = res.getHeaders();
+  const totalCountHeader = headers['x-total-count'];
+
+  if (totalCountHeader && req.method === 'GET') {
+    const queryParams = queryString.parse(req._parsedUrl.search);
+    // "?_page=1&_limit=3" -> {_page: '1', _limit='3'}
+
+    const result = {
+      data: res.locals.data,
+      pagination: {
+        _page: queryParams._page || 1,
+        _limit: queryParams._limit || 10,
+        _totalRows: totalCountHeader,
+      },
+    };
+
+    return res.jsonp(result);
+  }
+
+  // otherwise, keep default behavior
+  res.jsonp(res.locals.data);
+};
 
 // Use default router
 server.use('/api', router);
