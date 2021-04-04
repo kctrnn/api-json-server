edc7f24b7089b1ccf9b4281e29e28daafeaa6d32
kctrnn <kctrnn@gmail.com>
2021-04-04 15:44:20 +0700

    random productList with categoryList

--- generate-data.js
+++ generate-data.js
@@ -3,35 +3,61 @@ const fs = require('fs');
 
 // faker.locale = 'vi';
 
-const randomProductList = (n) => {
+const randomCategoryList = (n) => {
   if (n <= 0) return [];
 
-  const productList = [];
+  const categoryList = [];
 
-  // loop and push product
+  // loop and push category
   Array.from(new Array(n)).forEach(() => {
-    const newProduct = {
+    const category = {
       id: faker.datatype.uuid(),
-      name: faker.commerce.product(),
+      name: faker.commerce.department(),
       createAt: Date.now(),
       updateAt: Date.now(),
     };
 
-    productList.push(newProduct);
+    categoryList.push(category);
   });
+
+  return categoryList;
+};
+
+const randomProductList = (categoryList, numberOfProducts) => {
+  if (numberOfProducts <= 0) return [];
+
+  const productList = [];
+
+  for (const category of categoryList) {
+    Array.from(new Array(numberOfProducts)).forEach(() => {
+      const newProduct = {
+        categoryId: category.id,
+        id: faker.datatype.uuid(),
+        name: faker.commerce.product(),
+        price: faker.commerce.price(),
+        description: faker.commerce.productDescription(),
+        thumbnailUrl: faker.image.imageUrl(),
+        createAt: Date.now(),
+        updateAt: Date.now(),
+      };
+
+      productList.push(newProduct);
+    });
+  }
 
   return productList;
 };
 
-// IFFE
+// IIFE
 (() => {
   // random data
-  const productList = randomProductList(5);
+  const categoryList = randomCategoryList(4);
+  const productList = randomProductList(categoryList, 5);
 
   // prepare db object
   const db = {
     products: productList,
-    categories: [],
+    categories: categoryList,
   };
 
   // write db object to db.json
