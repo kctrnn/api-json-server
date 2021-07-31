5645b1449c0ec5839f2cd9f015b10ec5b1427b20
kctrnn <kctrnn@gmail.com>
2021-07-31 23:12:44 +0700

    Add cities and students

--- generate-data.js
+++ generate-data.js
@@ -1,6 +1,6 @@
-const faker = require('faker');
-const fs = require('fs');
-const casual = require('casual');
+const faker = require("faker");
+const fs = require("fs");
+const casual = require("casual");
 
 // faker.locale = 'vi';
 
@@ -71,25 +71,79 @@ const randomPostList = (n) => {
   return postList;
 };
 
+const randomStudentList = (n) => {
+  if (n <= 0) return [];
+
+  const studentList = [];
+
+  Array.from(new Array(n)).forEach(() => {
+    const newStudent = {
+      id: casual.uuid,
+      name: casual.full_name,
+      age: casual.integer(18, 27),
+      mark: Number.parseFloat(casual.double(3, 10).toFixed(1)),
+      gender: ["male", "female"][casual.integer(1, 100) % 2],
+      city: ["hcm", "hn", "dn", "ha", "pt"][casual.integer(1, 100) % 5],
+
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+    };
+
+    studentList.push(newStudent);
+  });
+
+  return studentList;
+};
+
+const cityList = [
+  {
+    code: "hcm",
+    name: "Hồ Chí Minh",
+  },
+
+  {
+    code: "hn",
+    name: "Hà Nội",
+  },
+
+  {
+    code: "dn",
+    name: "Đà Nẵng",
+  },
+
+  {
+    code: "ha",
+    name: "Hội An",
+  },
+
+  {
+    code: "pt",
+    name: "Phan Thiết",
+  },
+];
+
 // IIFE
 (() => {
   // random data
   const categoryList = randomCategoryList(4);
   const productList = randomProductList(categoryList, 5);
   const postList = randomPostList(50);
+  const studentList = randomStudentList(50);
 
   // prepare db object
   const db = {
     posts: postList,
     products: productList,
     categories: categoryList,
+    cities: cityList,
+    students: studentList,
   };
 
   // write db object to db.json
-  fs.writeFile('db.json', JSON.stringify(db), (err) => {
+  fs.writeFile("db.json", JSON.stringify(db), (err) => {
     if (err) {
       return console.log(err);
     }
-    console.log('Generate successfully!');
+    console.log("Generate successfully!");
   });
 })();
