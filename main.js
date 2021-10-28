5ddb6dd3a0f20dc26acb2f314858508a9503d797
Kim Chan <kctrnn@gmail.com>
2021-10-28 20:00:00 +0700

    Add login, register, profile route

--- main.js
+++ main.js
@@ -1,32 +1,149 @@
+require('dotenv').config();
 const jsonServer = require('json-server');
+const queryString = require('query-string');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const queryString = require('query-string');
+
+const yup = require('yup');
+const jwt = require('jsonwebtoken');
+const casual = require('casual');
+
+const PORT = process.env.PORT || 3000;
+const PRIVATE_KEY = 'ae9468ec-c1fe-4cce-9772-cd899a2b496a';
+const SECONDS_PER_DAY = 60 * 60 * 24;
 
 // Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares);
 
-// Add custom routes before JSON Server router
-server.get('/echo', (req, res) => {
-  res.jsonp(req.query);
-});
-
 // To handle POST, PUT and PATCH you need to use a body-parser
 // You can use the one used by JSON Server
 server.use(jsonServer.bodyParser);
-
 server.use((req, res, next) => {
-  if (req.method === 'POST') {
-    req.body.createdAt = Date.now();
-    req.body.updatedAt = Date.now();
-  } else if (req.method === 'PATCH') {
-    req.body.updatedAt = Date.now();
+  const now = Date.now();
+  switch (req.method) {
+    case 'POST': {
+      req.body.createdAt = now;
+      req.body.updatedAt = now;
+    }
+    case 'PATCH': {
+      req.body.updatedAt = now;
+    }
   }
-
   // Continue to JSON Server router
   next();
 });
+
+server.post('/api/login', async (req, res) => {
+  const loginSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required('Missing username')
+      .min(4, 'username should have at least 4 characters'),
+
+    password: yup
+      .string()
+      .required('Missing password')
+      .min(6, 'password should have at least 6 characters'),
+  });
+
+  try {
+    await loginSchema.validate(req.body);
+  } catch (error) {
+    res
+      .status(400)
+      .jsonp({ error: error.errors?.[0] || 'Invalid username or password' });
+  }
+
+  // validate username and password
+  const { username, password } = req.body;
+  const token = jwt.sign({ sub: username }, PRIVATE_KEY, {
+    expiresIn: SECONDS_PER_DAY,
+  });
+  const expiredAt = new Date(Date.now() + SECONDS_PER_DAY * 1000).getTime();
+
+  // if valid, generate a JWT and return, set it expired in 1 day
+  res.jsonp({ accessToken: token, expiredAt });
+});
+
+server.post('/api/register', async (req, res) => {
+  const registerSchema = yup.object().shape({
+    username: yup
+      .string()
+      .required('Missing username')
+      .min(4, 'username should have at least 4 characters'),
+
+    email: yup
+      .string()
+      .required('Missing email')
+      .email('please enter a valid email address'),
+
+    password: yup
+      .string()
+      .required('Missing password')
+      .min(6, 'password should have at least 6 characters'),
+  });
+
+  try {
+    await registerSchema.validate(req.body);
+  } catch (error) {
+    res
+      .status(400)
+      .jsonp({ error: error.errors?.[0] || 'Invalid username or password' });
+  }
+
+  // validate username and password
+  const { username, password } = req.body;
+  const token = jwt.sign({ sub: username }, PRIVATE_KEY, {
+    expiresIn: SECONDS_PER_DAY,
+  });
+  const expiredAt = new Date(Date.now() + SECONDS_PER_DAY * 1000).getTime();
+
+  // if valid, generate a JWT and return, set it expired in 1 day
+  res.jsonp({ accessToken: token, expiredAt });
+});
+
+function protectedRoute(req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader)
+      return res.status(401).json({ message: 'You need to login to access' });
+
+    const [tokenType, accessToken] = authHeader.split(' ');
+    if (tokenType !== 'Bearer') {
+      return res
+        .status(401)
+        .json({ message: 'Invalid token type. Only "Bearer" supported' });
+    }
+
+    jwt.verify(accessToken, PRIVATE_KEY);
+    next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ message: 'Access token is not valid or expired' });
+  }
+}
+
+server.get('/api/profile', protectedRoute, (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    const [tokenType, accessToken] = authHeader.split(' ');
+    const payload = jwt.decode(accessToken);
+
+    return res.status(200).json({
+      username: payload.sub,
+      name: casual.full_name,
+      email: casual.email.toLowerCase(),
+    });
+  } catch (error) {
+    console.log('failed to parse token', error);
+    return res.status(400).json({ message: 'Failed to parse token' });
+  }
+});
+
+// private apis
+server.use('/api/private', protectedRoute, router);
 
 router.render = (req, res) => {
   // check GET with pagination
@@ -56,9 +173,6 @@ router.render = (req, res) => {
 
 // Use default router
 server.use('/api', router);
-
-const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
   console.log('JSON Server is running');
 });
